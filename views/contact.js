6049043acb8da7725ca7cf6a595345c0a7896e78
Amninder Chahal <amninderchahal@Amninders-MacBook-Pro.local>
2018-04-05 21:20:59 -0400

    Migration to Vue.js initial commit

--- views/contact.js
+++
@@ -1,4 +0,0 @@
-angular.module("portfolio.contact", ["ngRoute"])
-.controller("contactCtrl", ["$scope", function($scope){
-    $scope.pageClass = "page-contact";
-}]);
--- views/work.js
+++
@@ -1,8 +0,0 @@
-angular.module("portfolio.work", ["ngRoute", "portfolio.directive.projectLink"])
-.controller('workCtrl', ["$scope", "$http", function($scope, $http){
-    $scope.pageClass = "page-work";
-    $http.get('/json/work.json')
-    .then(function(response){
-        $scope.projects = response.data.projects;
-    });
-}]);
